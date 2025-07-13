07d2037f29edc2979d9abbe7251fb5499d55a4ba
aishwary jhunjhunwala <aishwaryjjw@gmail.com>
2025-07-13 19:05:47 +0530

    Achievement Section Update

--- src/config/achievement.js
+++ src/config/achievement.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const achievementsData = [
   {
     title: "Name",
--- src/pages/playground/Achievements.jsx
+++ src/pages/playground/Achievements.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { achievementsData } from "../../config/achievement";
 
 export default function Achievements() {
