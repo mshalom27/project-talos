986c2a1226bb8788beb97e6a6841ef006028d596
Anwesha.28 <sarkar.anwesha28@gmail.com>
2025-08-16 11:02:23 +0530

    About Text,Minor fix in Events

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ import { siteConfig } from "../config/navbarHero";
 export default function HeroSection() {
   return (
     <section className="bg-white min-h-[400px]  pt-[80px] sm:pt-[100px] md:pt-[120px] lg:pt-[130px] px-4 sm:px-6 md:px-10 lg:px-16 py-12 sm:py-16">
-      <div className="max-w-[1600px] mx-auto flex flex-col md:flex-row items-center md:items-start justify-center md:justify-between">
+      <div className="max-w-[1600px] mx-auto flex flex-col md:flex-row items-center md:items-start justify-center md:justify-between md:gap-x-12">
         <div className="w-full max-w-[700px] px-2 md:px-0 space-y-4 text-center md:text-left">
           <p
             className="text-xl sm:text-2xl md:text-3xl font-normal text-black leading-[120%]"
@@ -46,7 +46,7 @@ export default function HeroSection() {
           </div>
         </div>
 
-        <div className="w-full max-w-[500px] px-2 md:px-0 mt-10 md:mt-0">
+        <div className="w-full max-w-[500px] px-2 md:px-0 mt-10 md:mt-0 ">
           <img
             src={siteConfig.hero.image}
             alt="ASME NIT Rourkela team members"
