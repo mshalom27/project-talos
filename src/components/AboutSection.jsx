0992e81a2e237104d701f973a63bf6785febd77b
mshalom27 <menshalom07@gmail.com>
2025-09-04 23:01:50 +0530

    transition

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,22 +1,44 @@
-import Button from "./shared/Button";
+import { useState, useEffect } from "react";
 import { siteConfig } from "../config/navbarHero";
 
 export default function AboutSection() {
+  const [showFirst, setShowFirst] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => setShowFirst((prev) => !prev), 4000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <section
       id="about"
       className="bg-white px-4 sm:px-6 md:px-10 lg:px-16 py-12 sm:py-16"
     >
-      <div className="w-full max-w-[1600px] mx-auto flex flex-col md:flex-row items-center justify-between gap-y-10 gap-x-10 xl:gap-x-12 2xl:gap-x-14">
-        <div className="w-full max-w-[500px] px-2 md:px-0 mt-10 md:mt-0">
+      <div className="w-full max-w-[1600px] mx-auto flex flex-col md:flex-row items-start md:items-center justify-between gap-x-10 xl:gap-x-12 2xl:gap-x-14">
+        <div className="w-full relative flex justify-center items-center h-[400px] md:h-[500px]">
           <img
             src={siteConfig.about.image}
-            alt="ASME NIT Rourkela team members"
-            className="rounded-md shadow-lg object-cover w-full h-auto"
+            alt="About image 1"
+            className="block md:hidden rounded-md shadow-lg object-cover w-full h-full"
+          />
+
+          <img
+            src={siteConfig.about.image}
+            alt="About image 1 fade"
+            className={`hidden md:block rounded-md shadow-lg object-cover w-full h-full absolute top-0 left-0 right-0 bottom-0 m-auto transition-opacity duration-1000 ${
+              showFirst ? "opacity-100 z-10" : "opacity-0 z-0"
+            }`}
+          />
+          <img
+            src={siteConfig.hero.image}
+            alt="About image 2 fade"
+            className={`hidden md:block rounded-md shadow-lg object-cover w-full h-full absolute top-0 left-0 right-0 bottom-0 m-auto transition-opacity duration-1000 ${
+              showFirst ? "opacity-0 z-0" : "opacity-100 z-10"
+            }`}
           />
         </div>
 
-        <div className="w-full  text-center md:text-left space-y-4 max-w-[700px]">
+        <div className="w-full text-center md:text-left space-y-4 max-w-[700px]">
           <h1
             className="text-3xl sm:text-4xl md:text-[47px] lg:text-[56px] font-bold uppercase text-black leading-[120%]"
             style={{ fontFamily: "Helvetica, Arial, sans-serif" }}
@@ -36,7 +58,7 @@ export default function AboutSection() {
               href="#cards"
               role="button"
               tabIndex={0}
-              className=" text-2xl h-12 justify-center items-center flex bg-[rgba(6,25,70,1)] text-[#ccc0c0] underline-offset-4 transition duration-300 ease-in-out hover:bg-none hover:bg-white hover:text-[#061946] font-semibold py-2 px-4 rounded"
+              className="text-2xl h-12 justify-center items-center flex bg-[rgba(6,25,70,1)] text-[#ccc0c0] underline-offset-4 transition duration-300 ease-in-out hover:bg-none hover:bg-white hover:text-[#061946] font-semibold py-2 px-4 rounded"
               style={{ fontFamily: "Helvetica, Arial, sans-serif" }}
             >
               {siteConfig.about.button}
